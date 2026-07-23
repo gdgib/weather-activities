cd20add981841d49a7cd6206812c9ba461c92a9e
Greg Gibeling <gdgib@outlook.com>
2026-07-23 12:22:21 -0700

    G2-1966 Terminate string properly

--- custom_components/weather-activities/binary_sensor.py
+++ custom_components/weather-activities/binary_sensor.py
@@ -283,12 +283,12 @@ class WeatherActivitiesDaySensor(WeatherActivitiesSensor):
                     hours_prev = hours_current
                 else:
                     explanation = self.explain_mismatch(next((forecast for forecast in filtered_activity if lambda fc: hadt.parse_datetime(fc.get(ATTR_FORECAST_TIME)) == hours_prev + dt.timedelta(hours=1)), None))
-                    hours_ranges.append(hours_start.strftime("%H:%M") + " to + hours_prev.strftime("%H:%M") + " because " + explanation)
+                    hours_ranges.append(hours_start.strftime("%H:%M") + " to " + hours_prev.strftime("%H:%M") + " because " + explanation)
                     hours_start = hours_current
                     hours_prev = hours_current
             if hours_start is not None:
                 explanation = self.explain_mismatch(next((forecast for forecast in filtered_activity if lambda fc: hadt.parse_datetime(fc.get(ATTR_FORECAST_TIME)) == hours_prev + dt.timedelta(hours=1)), None))
-                hours_ranges.append(hours_start.strftime("%H:%M") + " to + hours_prev.strftime("%H:%M") + " because " + explanation)
+                hours_ranges.append(hours_start.strftime("%H:%M") + " to " + hours_prev.strftime("%H:%M") + " because " + explanation)
             
             self._attr_extra_state_attributes = {
                 ATTR_HRS_COUNT: len(filtered_activity),
